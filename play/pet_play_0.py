12684b643c6020ebefd3fbac2f3574269b1e6e8b
Tianyang Li <tmy1018@gmail.com>
2012-11-11 21:47:53 +0800

    fjoiewar

--- play/pet_play_0.py
+++ play/pet_play_0.py
@@ -22,7 +22,10 @@ from util import blat_0
 
 def main():
     try:
-        opts, _ = getopt.getopt(sys.argv[1:], '', [])
+        opts, _ = getopt.getopt(sys.argv[1:],
+                                '',
+                                ['read1', 'read2',
+                                 'psl1', 'psl2'])
     except getopt.GetoptError as err:
         print >> sys.stderr, str(err)
         sys.exit(1)
