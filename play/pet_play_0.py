41715327d2022a7c583c3d5be617f265c3223b26
Tianyang Li <tmy1018@gmail.com>
2012-11-11 17:10:29 +0800

    gjiowejrjo

---
+++ play/pet_play_0.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+#  Copyright (C) 2012 Tianyang Li
+#  ty@li-tianyang.com
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+
+import getopt
+import sys
+
+def main():
+    try:
+        opts, _ = getopt.getopt(sys.argv[1:], '', [])
+    except getopt.GetoptError as err:
+        print >> sys.stderr, str(err)
+        sys.exit(1)
+
+
