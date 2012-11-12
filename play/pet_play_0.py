f94adbbdc96ef8d3f6241daa07b2c150bef90b4d
Tianyang Li <tmy1018@gmail.com>
2012-11-12 11:32:19 +0800

    gfewiorjiojo

--- play/pet_play_0.py
+++ play/pet_play_0.py
@@ -25,7 +25,7 @@ def main():
         opts, _ = getopt.getopt(sys.argv[1:],
                                 '',
                                 ['read1', 'read2',
-                                 'psl1', 'psl2'])
+                                 'psl1', 'psl2', 'gtf'])
     except getopt.GetoptError as err:
         print >> sys.stderr, str(err)
         sys.exit(1)
