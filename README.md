18f2bdb495dbcefcb2af63c4f93765ec5b6d5782
Tianyang Li <tmy1018@gmail.com>
2012-12-05 16:20:26 +0800

    gejiowajoij

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Estimate a transcript's length from RNA-Seq data
 
 ## Usage
 
-This program uses [BLAT](http://genome.ucsc.edu/FAQ/FAQblat.html) to align reads to a reference genome, 
+This program uses [TopHat](http://tophat.cbcb.umd.edu/) to align reads to a reference genome, 
 and estimate a transcript's start and end site.
 
 Utilities from [https://github.com/tianyang-li/misc-bioinfo-0](https://github.com/tianyang-li/misc-bioinfo-0) are used for data preprocessing. 
---
+++ play/blat_multihit_0.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+#  Copyright (C) 2012 Tianyang Li
+#  ty@li-tianyang.com
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+
+
+import getopt
+import sys
+
+from util.blat_0 import read_psl
+
+
+def main():
+    try:
+        opts, _ = getopt.getopt(sys.argv[1:], '', ['psl='])
+    except getopt.GetoptError as err:
+        print >> sys.stderr, str(err)
+        sys.exit(1)
+        
+    psl_file = None
+    
+    for opt, arg in opts:
+        if opt == '--psl':
+            psl_file = arg
+    
+    if (not psl_file):
+        print >> sys.stderr, "missing"
+        sys.exit(1)
+    
+    
+if __name__ == '__main__':
+    main()    
