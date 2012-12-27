6ac2d0789991b6039e92764f1f9e4b31374319ed
Tianyang Li <tmy1018@gmail.com>
2012-12-27 17:22:50 +0800

    test BLAT
    do some simulations to see what BLAT is like for RNA-PET

---
+++ play/blat_test_0/gen.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+#  Copyright (C) 2012 Tianyang Li
+#  ty@li-tianyang.com
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+
+
+import sys
+import getopt
+from random import randint, choice
+
+
+def main():
+    l, r, n = None, None, None
+    
+    try:
+        opts, _ = getopt.getopt(sys.argv[1:], 'l:r:n:')
+    except getopt.GetoptError as err:
+        print >> sys.stderr, str(err)
+        sys.exit(1)
+        
+    for opt, arg in opts:
+        if opt == '-l':
+            # subject seq length
+            l = int(arg)
+        if opt == '-r':
+            # read seq length
+            r = int(arg)
+        if opt == '-n':
+            # # of reads
+            n = int(arg)
+    
+    if (not l or not r or not n):
+        print >> sys.stderr, "missing"
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
+
